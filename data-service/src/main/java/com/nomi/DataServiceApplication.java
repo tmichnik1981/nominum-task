27c07e65a8455d6b1235232e85279d01d9f004be
Tomasz Michnik <tomasz_michnik@epam.com>
2018-04-26 15:20:30 +0200

    Added security

--- data-service/src/main/java/com/nomi/DataServiceApplication.java
+++ data-service/src/main/java/com/nomi/DataServiceApplication.java
@@ -2,8 +2,10 @@ package com.nomi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class DataServiceApplication {
 
 	public static void main(String[] args) {
